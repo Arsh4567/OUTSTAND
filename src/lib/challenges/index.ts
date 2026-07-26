7a5ad80105a0a12e850f481b2631665c39e41967
Arsh <sewasingh13111944@gmail.com>
2026-07-26 13:01:37 +0530

    Create index.ts to export challenges modules

---
+++ src/lib/challenges/index.ts
@@ -0,0 +1,6 @@
+// This file acts as a "barrel", exporting everything from our modular files
+// so the rest of the app can still import from "@/lib/challenges"
+
+export * from "./challenges.types";
+export * from "./challenges.styles";
+export * from "./challenges.data";
