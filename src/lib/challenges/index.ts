b6aba10a8c2b36dda227f6d23fd4712ca836196c
Arsh <sewasingh13111944@gmail.com>
2026-07-26 13:08:05 +0530

    Delete src/lib/challenges directory

--- src/lib/challenges/index.ts
+++
@@ -1,6 +0,0 @@
-// This file acts as a "barrel", exporting everything from our modular files
-// so the rest of the app can still import from "@/lib/challenges"
-
-export * from "./challenges.types";
-export * from "./challenges.styles";
-export * from "./challenges.data";
