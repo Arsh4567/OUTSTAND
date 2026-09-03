665270fc0b4ffd767b98f62536a1d1436dcb374e
Arsh <sewasingh13111944@gmail.com>
2026-09-03 18:02:13 +0530

    chore: remove dead onboarding redirect route

--- src/routes/_authenticated/onboarding.tsx
+++
@@ -1,5 +0,0 @@
-import { createFileRoute, redirect } from "@tanstack/react-router";
-
-export const Route = createFileRoute("/_authenticated/onboarding")({
-  beforeLoad: () => { throw redirect({ to: "/dashboard", replace: true }); },
-});
